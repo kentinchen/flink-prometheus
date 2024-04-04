327215125fed8b2700573ab618a3736ec9db7627
kentin <kentin.chen@gmail.com>
2024-04-04 14:58:03 +0800

    docker image修改

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,7 @@
+object Versions {
+    const val flinkVersion = "1.19.0"
+}
+
+object Libs {
+    val flink_java = "org.apache.flink:flink-java:${Versions.flinkVersion}"
+}
--- connector/flink-connector-pushgateway/build.gradle.kts
+++ connector/flink-connector-pushgateway/build.gradle.kts
@@ -9,7 +9,12 @@ repositories {
     mavenCentral()
 }
 
+val flinkVersion = "1.19.0"
+
 dependencies {
+    compileOnly("org.apache.flink:flink-java:$flinkVersion")
+    compileOnly("org.apache.flink:flink-streaming-java:$flinkVersion")
+
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
