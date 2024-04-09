426c8210b86b9c63fa39570c6e2fb83c23b0cad0
kentin <kentin.chen@gmail.com>
2024-04-09 23:55:49 +0800

    udf

--- deploy/dev/flink/gen-flink-sql.sh
+++ deploy/dev/flink/gen-flink-sql.sh
@@ -10,5 +10,7 @@ FLINK_OUTPUT_DIR=$SHELL_DIR/out
 echo "template dir: $FLINK_TEMPLATE_DIR"
 echo "output dir: $FLINK_OUTPUT_DIR"
 
+envsubst <"$FLINK_TEMPLATE_DIR"/gen-prom.tsql  >"$FLINK_OUTPUT_DIR"/gen-prom.sql
+envsubst <"$FLINK_TEMPLATE_DIR"/sls-prom-kv.tsql  >"$FLINK_OUTPUT_DIR"/sls-prom-kv.sql
 envsubst <"$FLINK_TEMPLATE_DIR"/sls-prom-ecs.tsql  >"$FLINK_OUTPUT_DIR"/sls-prom-ecs.sql
 envsubst <"$FLINK_TEMPLATE_DIR"/sls-prom-oss.tsql  >"$FLINK_OUTPUT_DIR"/sls-prom-oss.sql
