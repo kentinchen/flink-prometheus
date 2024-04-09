b95417c58081ccffee9fafc5e7849cc117658aca
kentin <kentin.chen@gmail.com>
2024-04-09 21:00:13 +0800

    udf

--- connectors/flink-connector-pushgateway/src/main/java/com/flink/connectors/pushgateway/config/ConnectorConfigConstants.java
+++ connectors/flink-connector-pushgateway/src/main/java/com/flink/connectors/pushgateway/config/ConnectorConfigConstants.java
@@ -10,49 +10,11 @@ import lombok.experimental.UtilityClass;
 public final class ConnectorConfigConstants {
     public static final String PROP_DELIM = ",";
 
-    /**
-     * A property prefix for http connector.
-     */
-    public static final String GID_CONNECTOR_HTTP = "connector.pushgateway.";
-
-    /**
-     * A property prefix for http connector header properties
-     */
-    public static final String SINK_HEADER_PREFIX = GID_CONNECTOR_HTTP + "sink.header.";
-    public static final String LOOKUP_SOURCE_HEADER_PREFIX = GID_CONNECTOR_HTTP + "source.lookup.header.";
-
-    /**
-     * Whether to use the raw value of the Authorization header. If set, it prevents
-     * the special treatment of the header for Basic Authentication, thus preserving the passed
-     * raw value. Defaults to false.
-     */
-    public static final String LOOKUP_SOURCE_HEADER_USE_RAW = GID_CONNECTOR_HTTP + "source.lookup.use-raw-authorization-header";
-
-    // --------- Error code handling configuration ---------
-    public static final String HTTP_ERROR_SINK_CODE_WHITE_LIST = GID_CONNECTOR_HTTP + "sink.error.code.exclude";
-    public static final String HTTP_ERROR_SINK_CODES_LIST = GID_CONNECTOR_HTTP + "sink.error.code";
-    public static final String HTTP_ERROR_SOURCE_LOOKUP_CODE_WHITE_LIST = GID_CONNECTOR_HTTP + "source.lookup.error.code.exclude";
-    public static final String HTTP_ERROR_SOURCE_LOOKUP_CODES_LIST = GID_CONNECTOR_HTTP + "source.lookup.error.code";
-    public static final String SINK_REQUEST_CALLBACK_IDENTIFIER = GID_CONNECTOR_HTTP + "sink.request-callback";
-    public static final String SOURCE_LOOKUP_QUERY_CREATOR_IDENTIFIER = GID_CONNECTOR_HTTP + "source.lookup.query-creator";
-
-    // -------------- HTTPS security settings --------------
-    public static final String ALLOW_SELF_SIGNED = GID_CONNECTOR_HTTP + "security.cert.server.allowSelfSigned";
-    public static final String SERVER_TRUSTED_CERT = GID_CONNECTOR_HTTP + "security.cert.server";
-    public static final String CLIENT_CERT = GID_CONNECTOR_HTTP + "security.cert.client";
-    public static final String CLIENT_PRIVATE_KEY = GID_CONNECTOR_HTTP + "security.key.client";
-    public static final String KEY_STORE_PATH = GID_CONNECTOR_HTTP + "security.keystore.path";
-    public static final String KEY_STORE_PASSWORD = GID_CONNECTOR_HTTP + "security.keystore.password";
-    public static final String KEY_STORE_TYPE = GID_CONNECTOR_HTTP + "security.keystore.type";
-
-    // ------ HTTPS timeouts and thread pool settings ------
-    public static final String LOOKUP_HTTP_TIMEOUT_SECONDS = GID_CONNECTOR_HTTP + "source.lookup.request.timeout";
-    public static final String SINK_HTTP_TIMEOUT_SECONDS = GID_CONNECTOR_HTTP + "sink.request.timeout";
-    public static final String LOOKUP_HTTP_PULING_THREAD_POOL_SIZE = GID_CONNECTOR_HTTP + "source.lookup.request.thread-pool.size";
-    public static final String LOOKUP_HTTP_RESPONSE_THREAD_POOL_SIZE = GID_CONNECTOR_HTTP + "source.lookup.response.thread-pool.size";
-    public static final String SINK_HTTP_WRITER_THREAD_POOL_SIZE = GID_CONNECTOR_HTTP + "sink.writer.thread-pool.size";
-
-    // ------ Sink request submitter settings ------
-    public static final String SINK_HTTP_REQUEST_MODE = GID_CONNECTOR_HTTP + "sink.writer.request.mode";
-    public static final String SINK_HTTP_BATCH_REQUEST_SIZE = GID_CONNECTOR_HTTP + "sink.request.batch.size";
+    public static final String CONST_PROM_JOB = "prom.job";
+    public static final String CONST_PROM_M_TYPE = "prom.m.type";
+    public static final String CONST_PROM_J_NAME = "prom.j.name";
+    public static final String CONST_PROM_M_NAME = "prom.m.name";
+    public static final String CONST_PROM_V_NAME = "prom.v.name";
+    public static final String CONST_PROM_H_NAME = "prom.h.name";
+    public static final String CONST_PROM_TS_NAME = "prom.ts.name";
 }
--- connectors/flink-connector-pushgateway/src/main/java/com/flink/connectors/pushgateway/table/pushgateway/PushgatewayDynamicSinkConnectorOptions.java
+++ connectors/flink-connector-pushgateway/src/main/java/com/flink/connectors/pushgateway/table/pushgateway/PushgatewayDynamicSinkConnectorOptions.java
@@ -3,7 +3,7 @@ package com.flink.connectors.pushgateway.table.pushgateway;
 import org.apache.flink.configuration.ConfigOption;
 import org.apache.flink.configuration.ConfigOptions;
 
-import static com.flink.connectors.pushgateway.config.ConnectorConfigConstants.SINK_REQUEST_CALLBACK_IDENTIFIER;
+import static com.flink.connectors.pushgateway.config.ConnectorConfigConstants.*;
 
 /**
  * Table API options for {@link PushgatewayDynamicSink}.
@@ -13,6 +13,18 @@ public class PushgatewayDynamicSinkConnectorOptions {
             ConfigOptions.key("pushgateway").stringType().noDefaultValue()
                     .withDescription("The pushgateway Url,for example：pushgateway:9091");
 
-    public static final ConfigOption<String> JOB_NAME = ConfigOptions.key("job.name").stringType()
-            .defaultValue("pushgateway");
+    public static final ConfigOption<String> PROM_JOB = ConfigOptions.key(CONST_PROM_JOB).stringType()
+            .defaultValue("flink").withDescription("作业标符");
+    public static final ConfigOption<String> PROM_M_TYPE = ConfigOptions.key(CONST_PROM_M_TYPE).stringType()
+            .defaultValue("m");
+    public static final ConfigOption<String> PROM_J_NAME = ConfigOptions.key(CONST_PROM_J_NAME).stringType()
+            .defaultValue("");
+    public static final ConfigOption<String> PROM_M_NAME = ConfigOptions.key(CONST_PROM_M_NAME).stringType()
+            .defaultValue("m");
+    public static final ConfigOption<String> PROM_V_NAME = ConfigOptions.key(CONST_PROM_V_NAME).stringType()
+            .defaultValue("v");
+    public static final ConfigOption<String> PROM_H_NAME = ConfigOptions.key(CONST_PROM_H_NAME).stringType()
+            .defaultValue("h");
+    public static final ConfigOption<String> PROM_TS_NAME = ConfigOptions.key(CONST_PROM_TS_NAME).stringType()
+            .defaultValue("ts");
 }
