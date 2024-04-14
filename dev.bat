ab0806191f9fdd6d53372e05cb307578e027f737
kentin <kentin.chen@gmail.com>
2024-04-14 16:45:31 +0800

    mixin

--- dev.bat
+++ dev.bat
@@ -31,3 +31,13 @@ foreach ($cntr in $cntrs) {
     docker exec $cntr mkdir -p /opt/flink/data;
 }
 
+http://localhost:8060/ui
+curl -XPOST http://localhost:8060/-/reload
+
+https://oapi.dingtalk.com/robot/send?access_token=9336388adedf15d84421a0a9347df939cdcaca77bc2970cd711e3140883bfb64
+SECfefb5520fdfb065b30fcee229f2415c4d9b74af285107c4379ccd49bf6ebf273
+
+https://oapi.dingtalk.com/robot/send?access_token=e342ee0d808536f5120b3370303710807594c43758f380fc565045149dd986d9
+告警
+
+curl -XPOST -d"{}" http://localhost:8060/dingtalk/webhook1/send
